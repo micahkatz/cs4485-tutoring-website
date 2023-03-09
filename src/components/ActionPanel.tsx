ec760878aad4710cad7f4eb3c0e59056af38ed3a
Justin <justinhardy2001@gmail.com>
2023-03-09 01:22:14 -0600

    Main Page action panel improvements, react icons replacing image assets

--- src/components/ActionPanel.tsx
+++ src/components/ActionPanel.tsx
@@ -1,31 +1,34 @@
 import React from 'react'
+import { IoCalendarClearOutline, IoCalendarClear, IoCalendarOutline, IoCalendar, IoBookmarksOutline, IoBookmarks, IoPersonOutline, IoPerson } from 'react-icons/io5'
 
 type Props = {}
 
 const ActionPanel = (props: Props) => {
     return (
-        <div className='bg-blue-200 h-60 relative'>
-            <div className="action-panel-layout">
-                <div className="action-panel-button">
-                    <span className="text-sm">Next Appointment</span>
-                    <img src="next_appointment.svg" className="action-panel-image"/>
-                    <p className="text-base">Feb. 17</p>
-                    <p className="text-sm">1pm • John Doe</p>
-                </div>
-                <div className="action-panel-button">
-                    <span className="text-sm">My Appointment</span>
-                    <img src="my_appointments.svg" className="action-panel-image"/>
-                    <p className="text-base">1 Scheduled</p>
-                </div>
-                <div className="action-panel-button">
-                    <span className="text-sm">My Bookmarks</span>
-                    <img src="my_bookmarks.png" className="action-panel-image"/>
-                    <p className="text-base">1 Saved</p>
-                </div>
-                <div className="action-panel-button">
-                    <span className="text-sm">My Profile</span>
-                    <img src="my_profile.png" className="action-panel-image"/>
-                    <p className="text-base">Micah Katz</p>
+        <div className='bg-blue-200 h-60 w-full'>
+            <div className="flex justify-center w-auto h-full">
+                <div className="grid grid-cols-4 align-middle">
+                    <div className="h-48 m-auto ml-8 mr-8 align-top text-center font-bold space-y-1 max-w-1">
+                        <span className="text-sm">Next Appointment</span>
+                        <IoCalendarClearOutline fontSize="7rem" className="m-auto"/>
+                        <p className="text-base">Feb. 17</p>
+                        <p className="text-sm">1pm • John Doe</p>
+                    </div>
+                    <div className="h-48 m-auto ml-8 mr-8 align-top text-center font-bold space-y-1 max-w-1">
+                        <span className="text-sm">My Appointment</span>
+                        <IoCalendarOutline size='7rem' className="m-auto"/>
+                        <p className="text-base">1 Scheduled</p>
+                    </div>
+                    <div className="h-48 m-auto ml-8 mr-8 align-top text-center font-bold space-y-1 max-w-1">
+                        <span className="text-sm">My Bookmarks</span>
+                        <IoBookmarksOutline size='7rem' className="m-auto"/>
+                        <p className="text-base">1 Saved</p>
+                    </div>
+                    <div className="h-48 m-auto ml-8 mr-8 align-top text-center font-bold space-y-1 max-w-1">
+                        <span className="text-sm">My Profile</span>
+                        <IoPersonOutline size='7rem' className="m-auto"/>
+                        <p className="text-base">Micah Katz</p>
+                    </div>
                 </div>
             </div>
         </div>
