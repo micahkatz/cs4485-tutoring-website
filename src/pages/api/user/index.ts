ba351e33aa629ec03a7e50d04507fa09d71d57bb
micahkatz <micahj2110@gmail.com>
2023-04-04 15:15:47 -0500

    added update and create for user

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -9,20 +9,44 @@ export default async function handler(
     res: NextApiResponse<any | string>
 ) {
     switch (req.method) {
-        case 'POST': // to update a user
+        case 'PUT': // to update a user
             const body = req.body as user;
 
-            body.first_name;
             try {
+                const { userID, ...withoutId } = body;
+
+                const createResponse = await prisma.user.create({
+                    data: withoutId,
+                });
+
+                res.status(200).json(createResponse);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Server Error');
+            }
+
+            break;
+        case 'POST': // to update a user
+            const partialBody = req.body as Partial<user>;
+
+            if (!partialBody.userID) {
+                res.status(400).send('Missing user ID');
+                break;
+            }
+
+            try {
+                const { userID, ...withoutId } = partialBody;
+
                 const updateResponse = await prisma.user.update({
                     where: {
-                        userID: body.userID,
+                        userID,
                     },
-                    data: body,
+                    data: withoutId,
                 });
 
                 res.status(200).json(updateResponse);
-            } catch {
+            } catch (error) {
+                console.error(error);
                 res.status(500).send('Server Error');
             }
 
