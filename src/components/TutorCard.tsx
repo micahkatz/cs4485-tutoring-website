440ebbc86ba055d96dff6e2b33ad08cc8ee362c7
Justin <justinhardy2001@gmail.com>
2023-04-05 03:57:03 -0500

    Quick tutor card fix

--- src/components/TutorCard.tsx
+++ src/components/TutorCard.tsx
@@ -29,7 +29,7 @@ const TutorCard = (props: Props) => {
                 }} />
                 <p className='text-lg w-full text-primary'>{userData.first_name} {userData.last_name}</p>
                 <p className='text-sm w-full h-full text-primary text-left line-clamp-3'>
-                    {tutorData.about_me || tutorData.about_me != '' ? tutorData.about_me : 'This tutor has not yet filled out their about me description. For more information, view their profile.'}
+                    {tutorData.about_me && tutorData.about_me != '' ? tutorData.about_me : 'This tutor has not yet filled out their about me description. For more information, view their profile.'}
                 </p>
                 <div className='w-full flex mt-2 gap-2 justify-between'>
                     <TagList tags={subjectData} />
