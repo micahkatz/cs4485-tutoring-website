fe78b6e5cfb5a9cc8f7e7b5ef668188297e7dab0
Justin <justinhardy2001@gmail.com>
2023-05-03 23:20:00 -0500

    Tutor Search by Day/Hour available

--- src/components/TutorFeed.tsx
+++ src/components/TutorFeed.tsx
@@ -1,32 +1,34 @@
 import React, { useEffect } from 'react'
 import { Oval } from 'react-loader-spinner'
 import TutorCard from '../components/TutorCard'
-import { tutor, user, subject, tutors_subjects } from '@prisma/client'
+import { tutor, user, subject, tutors_subjects, availability } from '@prisma/client'
 import { TutorWithSubjects } from '@/types/globals'
+import { truncate } from 'fs'
 
 
 type Props = {
     filterName: string;
     filterSubject: string;
     filterDay: Date | undefined;
-    filterHours: Date | undefined;
-    filterFavorites: boolean;
+    filterHour: Date | undefined;
 }
 
 const TutorFeed = (props: Props) => {
     const [tutorData, setTutorData] = React.useState<TutorWithSubjects[]>()
     const [tutorUserData, setTutorUserData] = React.useState<user[]>()
     const [tutorSubjectData, setTutorSubjectData] = React.useState<subject[][]>()
+    const [tutorAvailiabilityData, setTutorAvailabilityData] = React.useState<availability[][]>()
     const [tutorFavoriteRefs, setTutorFavoritesRefs] = React.useState<boolean[]>([])
     const [isLoading, setLoading] = React.useState<boolean>(true)
     const [loadError, setLoadError] = React.useState<boolean>(false)
     const [tutorDisplayIndexes, setTutorDisplayIndexes] = React.useState<number[]>([])
-    const nameFilter = props.filterName, subjectFilter = props.filterSubject, dayFilter = props.filterDay, hoursFilter = props.filterHours, filterFavorites = props.filterFavorites;
+    const [sortedTutorDisplayIndexes, setSortedTutorDisplayIndexes] = React.useState<number[]>([])
+    const nameFilter = props.filterName, subjectFilter = props.filterSubject, dayFilter = props.filterDay, hourFilter = props.filterHour;
     
     const fetchTutorData = async () => {
         await fetch('api/tutor', {method: 'GET'})
         .then((resp) => resp.json())
-        .then((json) => {
+        .then(async (json) => {
             // read json as tutor array
             const result = json as TutorWithSubjects[]
 
@@ -39,10 +41,13 @@ const TutorFeed = (props: Props) => {
             // Check if tutor data is not undefined
             if(result) {
                 // Fetch user data
-                fetchTutorUserData(result)
+                await fetchTutorUserData(result)
 
                 // Fetch subject data
-                fetchTutorSubjectData(result)
+                await fetchTutorSubjectData(result)
+
+                // Fetch availability data
+                await fetchTutorAvailability(result)
 
                 // Update data state
                 setTutorData(result)
@@ -55,6 +60,9 @@ const TutorFeed = (props: Props) => {
                 for(let i = 0; i < result.length; i++)
                     indexes.push(i)
                 setTutorDisplayIndexes(indexes)
+
+                // Set loading
+                setLoading(false)
             }
         })
         .catch((error) => {
@@ -141,9 +149,44 @@ const TutorFeed = (props: Props) => {
 
         // Update subject state
         setTutorSubjectData(subjectLists)
+    }
 
-        // Stop loading
-        setLoading(false)
+    const fetchTutorAvailability = async (tutors: TutorWithSubjects[]) => {
+        // Get availabilities
+        // Get tutor's user data
+        const map = new Map<number, availability[]>()
+        await Promise.all(tutors.map(async (tut: tutor) => {
+            await fetch(`api/availability?tutorId=${tut.fk_userID}`, {method: 'GET'})
+            .then((resp) => resp.json())
+            .then((json) => json as availability[])
+            .then((result) => {
+                // Map userId to user
+                map.set(tut.fk_userID, result)
+            })
+            .catch((error) => {
+                setLoadError(true)
+                setLoading(false)
+                console.log('error loading tutor\'s user data')
+                return
+            })
+        }))
+
+        // Get mappings (likely out-of-order from time they were fetched)
+        let availsList: availability[][] = []
+        tutors.forEach( (tut: tutor) => {
+            // use tutor foreign key to put user data in array parallel to tutor data array
+            const a = map.get(tut.fk_userID)
+            if (a) {
+                availsList.push(a)
+            }
+            else {
+                console.log('error loading availability')
+                console.log(a)
+            }
+        } )
+
+        // Update user data state
+        setTutorAvailabilityData(availsList)
     }
 
     const filterTutors = () => {
@@ -157,81 +200,119 @@ const TutorFeed = (props: Props) => {
         }
 
         let indexes: number[] = []
-        let blacklist: number[] = []
+        for(let i = 0; i < tutorData.length; i++) {
+            indexes.push(i)
+        }
 
-        // Parse filters
+        // Parse Filters
         /// Name
         for(let i = 0; i < tutorUserData.length; i++) {
             let name = (tutorUserData[i].first_name.toLowerCase() + " " + tutorUserData[i].last_name.toLowerCase())
-            if( name.indexOf(nameFilter.toLowerCase()) != -1 ) {
-                indexes.push(i) // NOTE: everything will be pushed if nameFilter is empty (empty value: '')
-            }
-            else { 
-                if( indexes.indexOf(i) != -1 ) {
+            if( indexes.indexOf(i) != -1 ) {
+                if( name.indexOf(nameFilter.toLowerCase()) == -1 ) {
                     indexes.splice(indexes.indexOf(i), 1)
                 }
-                blacklist.push(i)
             }
         }
 
         /// Subject
         if(subjectFilter != '') {
-            for(let i = 0; i < tutorSubjectData.length; i++) {
+            for( let i = 0; i < tutorSubjectData.length; i++ ) {
                 let subjects = tutorSubjectData[i]
-                // If tutor has subjects
-                if( subjects.length > 0 ) {
-                    for(let j = 0; j < subjects.length; j++) {
-                        if( subjects[j].name == subjectFilter ) {
-                            // Subject matches, break
-                            if( indexes.indexOf(i) == -1 && blacklist.indexOf(i) == -1 )
-                                // Add to display indexes since it's not already there
-                                indexes.push(i)
+
+                // See if tutor tutors for the filtered subject
+                let found = false
+                for( let j = 0; j < subjects.length; j++ ) {
+                    if( subjects[j].name == subjectFilter ) {
+                        found = true
+                        break
+                    }
+                }
+
+                // Remove if not found
+                if( !found ) {
+                    if( indexes.indexOf(i) != -1 ) {
+                        indexes.splice(indexes.indexOf(i), 1)
+                    }
+                }
+            }
+        }
+
+        /// Day
+        if( dayFilter ) {
+            // Put data from dayFilter into ease-of-access vars
+            const dayOfTheWeek = dayFilter.getDay()
+            const dayOfTheMonth = dayFilter.getDate()
+            const month = dayFilter.getMonth()
+            const year = dayFilter.getFullYear()
+
+            for( let i = 0; i < tutorAvailiabilityData.length; i++ ) {
+                const availabilities = tutorAvailiabilityData[i]
+
+                // See if tutor tutors on the selected day
+                let found = false
+                for( let j = 0; j < availabilities.length; j++ ) {
+                    const startDT = new Date(availabilities[j].startDT)
+                    const endDT = new Date(availabilities[j].endDT)
+                    if( availabilities[j].repeatWeekly ) {
+                        if( startDT.getDay() == dayOfTheWeek ) {
+                            found = true
                             break
                         }
-                        else {
-                            if( indexes.indexOf(i) != -1 ) {
-                                // Subject doesn't match, remove from display indexes
-                                indexes.splice(indexes.indexOf(i), 1)
-                            }
-
-                            // Add to blacklist if not already there
-                            if( blacklist.indexOf(i) != -1 )
-                                blacklist.push(i)
+                    }
+                    else {
+                        if(startDT.getDate() == dayOfTheMonth && startDT.getMonth() == month && startDT.getFullYear() == year) {
+                            found = true
+                            break
                         }
                     }
                 }
-                // If tutor has no subjects
-                else if( indexes.indexOf(i) != -1 ) {
-                    // Remove from display indexes if there already
-                    indexes.splice(indexes.indexOf(i), 1)
+
+                if( !found ) {
+                    if( indexes.indexOf(i) != -1 ) {
+                        indexes.splice(indexes.indexOf(i), 1)
+                    }
                 }
             }
         }
-        
-        /// Day
 
-        /// Hours
+        /// Hour
+        if( hourFilter ) {
+            // Put data from dayFilter into ease-of-access vars
+            const hour = hourFilter.getHours()
 
-        /// Favorites
+            for( let i = 0; i < tutorAvailiabilityData.length; i++ ) {
+                const availabilities = tutorAvailiabilityData[i]
 
-        // Set new display indexes
-        let sortedIndexes: number[] = []
-        for( let i = 0; i < indexes.length; i++ ) {
-            console.log(tutorFavoriteRefs[indexes[i]])
-            if( tutorFavoriteRefs[indexes[i]] ) {
-                sortedIndexes.unshift(indexes[i])
-            }
-            else {
-                sortedIndexes.push(indexes[i])
+                // See if tutor tutors on the selected day
+                let found = false
+                for( let j = 0; j < availabilities.length; j++ ) {
+                    const startDT = new Date(availabilities[j].startDT)
+                    const endDT = new Date(availabilities[j].endDT)
+                    if( availabilities[j].repeatWeekly ) {
+                        console.log(hour, 'vs', startDT.getHours(), '&', endDT.getHours())
+                        if( startDT.getHours() <= hour && endDT.getHours() >= hour ) {
+                            found = true
+                            break
+                        }
+                    }
+                }
+
+                if( !found ) {
+                    if( indexes.indexOf(i) != -1 ) {
+                        indexes.splice(indexes.indexOf(i), 1)
+                    }
+                }
             }
         }
-        setTutorDisplayIndexes(sortedIndexes)
+
+        setTutorDisplayIndexes(indexes)
     }
 
     const sortIndexes = () => {
         let sortedIndexes: number[] = []
         for( let i = 0; i < tutorDisplayIndexes.length; i++ ) {
-            console.log(tutorFavoriteRefs[tutorDisplayIndexes[i]])
+            //console.log(tutorFavoriteRefs[tutorDisplayIndexes[i]])
             if( tutorFavoriteRefs[tutorDisplayIndexes[i]] ) {
                 sortedIndexes.unshift(tutorDisplayIndexes[i])
             }
@@ -239,7 +320,7 @@ const TutorFeed = (props: Props) => {
                 sortedIndexes.push(tutorDisplayIndexes[i])
             }
         }
-        setTutorDisplayIndexes(sortedIndexes)
+        setSortedTutorDisplayIndexes(sortedIndexes)
     }
 
     useEffect(() => {
@@ -248,7 +329,11 @@ const TutorFeed = (props: Props) => {
 
     useEffect(() => {
         filterTutors()
-    }, [nameFilter, subjectFilter, dayFilter, hoursFilter])
+    }, [nameFilter, subjectFilter, dayFilter, hourFilter])
+
+    useEffect(() => {
+        sortIndexes()
+    }, [tutorDisplayIndexes])
 
     if( isLoading ) {
         return <div className='mt-6'><Oval width='75' color='#9748FF' secondaryColor='#BCE3FF'/></div>
@@ -260,7 +345,7 @@ const TutorFeed = (props: Props) => {
         return (
             <div className='flex justify-center'>
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 w-full sm:max-w-[75%] 2xl:max-w-[66%]'>
-                    {tutorDisplayIndexes.map(index => (
+                    {sortedTutorDisplayIndexes.map(index => (
                             <TutorCard key={tutorData[index].fk_userID} 
                             tutor={tutorData[index]} 
                             user={tutorUserData[index]} 
